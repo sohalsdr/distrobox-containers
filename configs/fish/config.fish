a494c79c18da52408aeff01d6ca2804f7c98e2cd
sohalsdr <sohal@sohalsdr.com>
2024-12-07 04:04:53 -0500

    Added alias to post install script

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -6,4 +6,7 @@ if not command -v hx >/dev/null
     alias hx='helix'
 end
 
+# Create alias for post install script
+alias post-install-script='fish $HOME/.post-install/post-install.fish'
+
 starship init fish | source
