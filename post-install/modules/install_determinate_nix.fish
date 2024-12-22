a3c23f4e88bb0642825d2621225cc12ccafb13d1
sohalsdr <sohal@sohalsdr.com>
2024-12-22 08:03:56 +0530

    Added more args to install determinate nix

--- post-install/modules/install_determinate_nix.fish
+++ post-install/modules/install_determinate_nix.fish
@@ -3,7 +3,7 @@ function install_determinate_nix
     script_section_head "Install Package Manager: Determinate Nix"
     echo ""
     if gum confirm "Install the Nix Package Manager using the Determinate Nix Installer?"
-        if gum spin --title "Installing Nix" --show-output -- curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+        if gum spin --title "Installing Nix" --show-output -- curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --init none
             gum log -sl info "Successfully installed yay!"
         else
             gum log -sl error "Unable to install Nix."
